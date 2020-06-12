17d5e779c050833cb8fe5412297b93e88aae58af
John Tucker <johntucker0686@gmail.com>
2020-06-12 16:55:57 -0700

    forever since pushing

---
+++ app/components/OfflineNotice.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+import Text from "./Text";
+import colors from "../config/colors";
+
+const OfflineNotice = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>No Internet Connection</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    height: 50,
+    justifyContent: "center",
+    position: "absolute",
+    top: Constants.statusBarHeight,
+    width: "100%",
+    zIndex: 1,
+  },
+  text: { color: colors.white },
+});
+
+export default OfflineNotice;
